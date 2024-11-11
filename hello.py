211d32867fc65532fc0c944fd4cb26adb53a9eef
sriman3 <149222484+sriman3@users.noreply.github.com>
2024-11-11 16:15:14 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,34 @@
-def factorial(n):
-  if n==0:
-     result=n*factorial(n-1)
-     return result
-     
-n=int(input("enter the number"))
-print(factorial(n))
+class Cciitm():
+  def senapathy(self,name,age,dept):
+    self.name=name
+    self.age=age
+    self.dept=dept
+    
+
+class Network():
+  def acess(self,username,id,password):
+    self.username=username
+    self.id=id
+    self.password=password
+    
+
+class Technical():
+  def server(self,ram,rom,speed):
+    self.ram=ram
+    self.rom=rom
+    self.speed=speed
+
+iit=Cciitm("sriman",21,"project developer")
+print(iit.name())
+
+
+data=Network("sriman57","icsr39780","sriman@123")
+print(data.id())
+
+ware=Techincal('8gb','128gb',129.34.450)
+print(ware.speed())
+
+
+
+
+    
